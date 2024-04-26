d983b5719132e5d3722a85535351e3f505029a16
Marija Jovanovic <marijajovanovic671@gmail.com>
2024-04-26 13:52:40 +0200

    fatching data using data loader

--- react-proj/src/pages/JobPage.jsx
+++ react-proj/src/pages/JobPage.jsx
@@ -1,32 +1,16 @@
-import {useState,useEffect} from 'react';
-import {useParams} from 'react-router-dom';
-import Spinner from '../components/Spinner';
+import {useParams, useLoaderData} from 'react-router-dom';
 
 const JobPage = () => {
-
     const { id } = useParams();
-    const [job,setJob] = useState(null);
-    const [loading,setLoading] = useState(true);
-
-    useEffect(()=>{
-        const fetchJob = async () => {
-            try
-            {
-              const res = await fetch(`/api/jobs/${id}`);
-              const data = await res.json();
-              console.log(data);
-              setJob(data);
-            } catch(error)
-            {
-              console.log('Error fatching data',error);
-            } finally {
-                setLoading(false);
-            }
-        }
-        fetchJob();
-    },[])
+    const job = useLoaderData();
     
-  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+  return <h1>{job.title}</h1>;
 };
 
-export default JobPage
+//prosledimo objekat sa parametrima
+const jobLoader = async ({params}) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
+}
+export { JobPage as default, jobLoader}
